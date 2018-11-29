09fd386eb14836f7a0410a62ad8fd7fe6a0ccc65
Brian Mwathi <mwathibrian7@gmail.com>
2018-11-29 22:53:09 +0300

    For loop alternative initialization

--- 01-program-structure/triangle_loop.js
+++ 01-program-structure/triangle_loop.js
@@ -9,7 +9,12 @@
  * #######
  */
 let octothorpe = '';
-for(let i = 0; i < 7; i++) {
+for (let i = 0; i < 7; i++) {
     octothorpe += "#";
     console.log(octothorpe);
+}
+
+// Alternative
+for (let line = "#"; line.length <= 7; line += "#") {
+    console.log(line);
 }
